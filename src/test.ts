9bbbc47cefb25e095bc285a2830c7a9ce2e5ea07
SDX24 <161406791+SDX24@users.noreply.github.com>
2025-04-29 23:33:46 -0700

    start game model

---
+++ src/test.ts
@@ -0,0 +1,21 @@
+import Map from "./areas/Map/Map";
+import Game from "./areas/Types/Game";
+import Player from "./areas/Types/Player";
+
+const game = new Game();
+
+game.startGame(4, "ABC123");
+
+console.log("Game started with players:")
+console.log(game.players)
+
+console.log("Players are at positions:")
+console.log(game.map.playerPositions)
+
+console.log("Map tile 0:")
+console.log(game.map.tiles[0])
+
+console.log("Map tile 1:")
+console.log(game.map.tiles[1])
+
+//note the player positions are empty because tiles have them. they need to be link somehow TODO
