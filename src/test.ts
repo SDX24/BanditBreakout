49d4a205655d70c7765c81a75b38f285fb320f2e
SDX24 <161406791+SDX24@users.noreply.github.com>
2025-04-30 19:29:05 -0700

    change structure add gold tile

--- src/test.ts
+++ src/test.ts
@@ -20,14 +20,17 @@ console.log(tile0)
 console.log("Map tile 1:")
 console.log(game.map.tiles[1])
 
-game.move.allTo(1)
+
+console.log("***")
+console.log("***")
+console.log("***")
+
+console.log("Testing event")
+let p1 = game.players[0]
+console.log(p1.getGold())
+console.log("Player 1 steping on safe")
 game.move.playerTo(1, 2)
-
-console.log("Map tile 1:")
-console.log(game.map.tiles[1])
-
-console.log("Map tile 2:")
-console.log(game.map.tiles[2])
+console.log(p1.getGold())
 
 
 //note the player positions are empty because tiles have them. they need to be link somehow TODO
