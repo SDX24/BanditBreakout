f8ece33e6c22a5ca2bf3d0660dbf00540f5ecef2
Lam Thai <tuoclamthai@gmail.com>
2025-04-30 22:08:06 -0700

    add background of the start screen

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import cmdZLogo from "./assets/CMD Z Logo.svg";
 import banditLogo from "./assets/Bandit Logo Circle.svg";
+import titleBackground from "./assets/title background.png";
 
 import "./App.css";
 
@@ -10,10 +11,12 @@ function App() {
   useEffect(() => {
     const timer1 = setTimeout(() => setCurrentScreen(2), 3000); // Change to screen 2 after 3s
     const timer2 = setTimeout(() => setCurrentScreen(3), 4000); // Change to screen 3 after 4s
+    const timer3 = setTimeout(() => setCurrentScreen(4), 6000); // Change back to screen 1 after 4s
 
     return () => {
       clearTimeout(timer1);
       clearTimeout(timer2);
+      clearTimeout(timer3);
     };
   }, []);
 
@@ -33,6 +36,14 @@ function App() {
 
       {currentScreen === 3 && (
         <img src={banditLogo} className="logo" alt="Bandit Logo" />
+      )}
+
+      {currentScreen === 4 && (
+        <img
+          src={titleBackground}
+          className="title-background"
+          alt="Title Background"
+        />
       )}
     </div>
   );
