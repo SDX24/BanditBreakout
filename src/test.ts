f57a00e2d046453052611bd56ce143eb8c5a433c
SDX24 <161406791+SDX24@users.noreply.github.com>
2025-05-01 23:45:49 -0700

    item system working

--- src/test.ts
+++ src/test.ts
@@ -6,6 +6,29 @@ const game = new Game();
 
 game.startGame(4, "ABC123");
 
+let p1 = game.players[0]
+
+let tile0 = game.map.tiles[0]
+let tile3 = game.map.tiles[3]
+let tile6 = game.map.tiles[6]
+console.log("Map tile 3:")
+console.log(tile3.playersOnTile)
+    
+p1.inventory.obtain(8)
+p1.inventory.items[0].use()
+
+console.log("Map tile 3:")
+console.log(tile3.playersOnTile)
+
+console.log("Map tile 6:")
+console.log(tile6.playersOnTile)
+
+p1.inventory.obtain(8)
+p1.inventory.useItem(8)
+
+console.log("Map tile 6:")
+console.log(tile6.playersOnTile)
+
 // console.log("Game started with players:")
 // console.log(game.players)
 
@@ -17,31 +40,29 @@ game.startGame(4, "ABC123");
 // console.log("Map tile 0:")
 // console.log(tile0)
 
-console.log("map tile 5 players:")
-console.log(game.map.tiles[5].playersOnTile)
+// console.log("map tile 5 players:")
+// console.log(game.map.tiles[5].playersOnTile)
 
-let p1 = game.players[0]
-p1.move.front(5)
+// let p1 = game.players[0]
+// p1.move.front(5)
 
-console.log("map tile 5 players:")
-console.log(game.map.tiles[5].playersOnTile)
+// console.log("map tile 5 players:")
+// console.log(game.map.tiles[5].playersOnTile)
 
-console.log("map tile 2 players:")
-console.log(game.map.tiles[2].playersOnTile)
+// console.log("map tile 2 players:")
+// console.log(game.map.tiles[2].playersOnTile)
 
-let p2 = game.players[1]
-p2.move.to(2)
+// let p2 = game.players[1]
+// p2.move.to(2)
 
-console.log("map tile 2 players:")
-console.log(game.map.tiles[2].playersOnTile)
+// console.log("map tile 2 players:")
+// console.log(game.map.tiles[2].playersOnTile)
 
-p1.move.back(1)
+// p1.move.back(1)
 
-console.log("map tile 4 players:")
-console.log(game.map.tiles[4].playersOnTile)
+// console.log("map tile 4 players:")
+// console.log(game.map.tiles[4].playersOnTile)
 
-p1.inventory.obtain(8)
-p1.inventory.items[0].inventory
 
 
 
