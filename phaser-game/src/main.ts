37675206ab70e3594b5a9d6d310a7256bafc4bda
Lam Thai <tuoclamthai@gmail.com>
2025-05-02 21:20:10 -0700

    create phaser

---
+++ phaser-game/src/main.ts
@@ -0,0 +1,21 @@
+import { GroupLogo } from "./scenes/Group-Logo";
+import { Logo } from "./scenes/Logo";
+import { Start } from "./scenes/Start";
+
+const config = {
+  type: Phaser.AUTO,
+  title: "Bandit Breakout",
+  description: "Turn-based RGP",
+  parent: "game-container",
+  width: 1280,
+  height: 720,
+  backgroundColor: "#000000",
+  pixelArt: false,
+  scene: [Start, GroupLogo, Logo],
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+};
+
+new Phaser.Game(config);
