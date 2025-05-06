e81d5fb5fb9627679a49685026add2976c0d9391
Lam Thai <tuoclamthai@gmail.com>
2025-05-05 23:19:03 -0700

    updated all the elements - no function in main screen

--- phaser-game/src/scenes/MainScreen.ts
+++ phaser-game/src/scenes/MainScreen.ts
@@ -1,4 +1,5 @@
 import Phaser from "phaser";
+import WebFontLoader from "webfontloader";
 
 export class MainScreen extends Phaser.Scene {
   private background!: Phaser.GameObjects.Image;
@@ -13,18 +14,69 @@ export class MainScreen extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("background", "assets/title background.png");
+    this.load.image("background", "assets/background.png");
     this.load.image("pole", "assets/pole.png");
     this.load.image("title-card", "assets/title card.png");
     this.load.image("start", "assets/start.png");
     this.load.image("options", "assets/options.png");
     this.load.image("quit", "assets/quit.png");
+
+    WebFontLoader.load({
+      custom: {
+        families: ["Wellfleet", "WBB"],
+        urls: ["/fonts.css"],
+      },
+    });
   }
 
   create() {
     const screen = this.add.container(0, 0);
 
-    const background = this.add.image(0, 0, "background");
+    const background = this.add.image(960, 540, "background");
     screen.add(background);
+
+    const optionsContainer = this.add.container(0, 0);
+    const options = this.add.image(960, 540, "options");
+    const optionsText = this.add.text(570, 805, "Settings", {
+      fontFamily: "WBB",
+      fontSize: 120,
+      color: "#492807",
+    });
+    optionsContainer.add(options);
+
+    const poleContainer = this.add.container(0, 0);
+    const pole = this.add.image(860, 540, "pole");
+    pole.setDisplaySize(1600, 1080);
+    poleContainer.add(pole);
+
+    const titleContainer = this.add.container(0, 0);
+    const titleCard = this.add.image(960, 540, "title-card");
+    titleCard.setDisplaySize(1920, 1000);
+    titleContainer.add(titleCard);
+    const titleCardText = this.add.text(450, 190, "Bandit Breakout", {
+      fontFamily: "WBB",
+      fontSize: 250,
+      color: "#492807",
+    });
+    titleContainer.add(titleCardText);
+
+    const start = this.add.image(940, 540, "start");
+    const startText = this.add.text(550, 670, "Start", {
+      fontFamily: "WBB",
+      fontSize: 100,
+      color: "#492807",
+    });
+    titleContainer.add(start);
+    titleContainer.add(startText);
+
+    const quit = this.add.image(940, 540, "quit");
+    const quitText = this.add.text(120, 910, "Quit", {
+      fontFamily: "WBB",
+      fontSize: 120,
+      color: "#492807",
+    });
+
+    titleContainer.add(quit);
+    titleContainer.add(quitText);
   }
 }
