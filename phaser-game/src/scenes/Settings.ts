69c0e86b5101e37ee06bde3fbfe87c3b5e886975
SDX24 <161406791+SDX24@users.noreply.github.com>
2025-05-05 21:18:38 -0700

    working on volumes

--- phaser-game/src/scenes/Settings.ts
+++ phaser-game/src/scenes/Settings.ts
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 import { Settings } from "backend/areas/Types/Settings"
 import WebFontLoader from "webfontloader";
 import { get } from "http";
+import { set } from "mongoose";
 
 export class SettingsMenu extends Phaser.Scene {
 
@@ -12,9 +13,11 @@ export class SettingsMenu extends Phaser.Scene {
   preload() {
     this.load.image("board", "tempAssets/settingsOverlay/board.png");
     this.load.svg("muteIcon", "tempAssets/settingsOverlay/Mute.svg");
-    // this.load.svg("sliderIcon", "tempAssets/settings/Slider.svg");
+    this.load.svg("slider", "tempAssets/settingsOverlay/Slider.svg");
+    this.load.svg("slider_button", "tempAssets/settingsOverlay/Slider_button.svg");
     this.load.svg("soundIcon", "tempAssets/settingsOverlay/Sound.svg");
     this.load.svg("backboard", "tempAssets/settingsOverlay/backboard.svg")
+    this.load.image("ovelayBackSign", "tempAssets/settingsOverlay/backSign.png");
 
 
     WebFontLoader.load({
@@ -55,67 +58,132 @@ export class SettingsMenu extends Phaser.Scene {
         }
   }
 
+const backboards: any[] = []
+backboardTexts.forEach((text, index) => {
+    const yOffset = getBackboardsOffset(index);
+    const backboard = this.add.image(0, yOffset, "backboard")
+    backboard.setDisplaySize(backboard.width * 3, backboard.height * 2.2);
+
+    const backboardText = this.add.text(0, yOffset, text, {
+        fontFamily: "WBB",
+        fontSize: 125,
+        color: "#ffffff",
+        align: "center",
+    }).setOrigin(0.5);
+
+    containerMain.add(backboard);
+    containerMain.add(backboardText);
+
+    backboard.setInteractive(new Phaser.Geom.Rectangle(0, 20, backboard.displayWidth, backboard.displayHeight), Phaser.Geom.Rectangle.Contains);
+    backboard.on("pointerdown", () => {
+        if (text === "Close options") {
+            closeSettings();
+        } else if (text === "Legend") {
+            openLayout(text);
+        } else if (text === "Settings") {
+            openLayout(text);
+        } else if (text === "Leave game") {
+            closeSettings(); // this should work when game is there
+        }
+    });
+
+    backboards.push(backboard, backboardText);
+});
+containerScene.add(containerMain);
+
+
+const openLayout = (layoutName: string) => {
+  containerMain.setVisible(false);
+
+  const layoutContainer = this.add.container(960, 540);
+  const layoutBoard = this.add.image(0, 0, "board");
+  layoutContainer.add(layoutBoard);
+
+
+
+  if (layoutName === "Legend") {
+
+      layoutContainer.add([]);
+  } else if (layoutName === "Settings") {
+
+      const settingsLabel = this.add.text(0, -200, "Settings", {
+          fontFamily: "Wellfleet",
+          fontSize: 61,
+          color: "#492807",
+      }).setOrigin(0.5);
+
+      const soundContainer = this.add.container(-400, 150);
+      const volumeTexts = ["Music", "SFX", "Voice"];
+
+      volumeTexts.forEach((text, index) => {
+        const yOffset = getBackboardsOffset(index);
+        const soundIcon = this.add.image(100, yOffset, "soundIcon")
+        soundIcon.setDisplaySize(soundIcon.width * 0.6, soundIcon.height * 0.6);
+        const volumeText = this.add.text(-200, yOffset-40, text, {
+          fontFamily: "Wellfleet",
+          fontSize: 61,
+          color: "#ffffff",
+          align: "center",
+      })
+      const volumeContainer = this.add.container(-400, 150);
+      const volumeSlider = this.add.image(0, yOffset, "slider")
+      const volumeSliderButton = this.add.image(0, yOffset, "slider_button")
+      volumeContainer.add([volumeSlider, volumeSliderButton]);
+      volumeContainer.setDisplaySize(volumeContainer.width * 0.6, volumeContainer.height * 0.6);
+
+      soundContainer.add([soundIcon, volumeText, volumeContainer]);
+      layoutContainer.add(soundContainer);
+
+      soundIcon.setInteractive();
+      soundIcon.on("pointerdown", () => {
+        if (text === "Music") {
+          console.log("Music");
+        } else if (text === "SFX") {
+          console.log("SFX");
+        } else if (text === "Voice") {
+          console.log("Voice");
+        }
+      });
+      });
+ 
+      layoutContainer.add([settingsLabel]);
+  } else if (layoutName === "Rules") {
+
+      layoutContainer.add([]);
+  }
+
+
+
+  const containerBackButton = this.add.container(-900, -350);
+  const overlayBackSign = this.add.image(0, 0, "ovelayBackSign");
+  overlayBackSign.setDisplaySize(overlayBackSign.width * 0.3, overlayBackSign.height * 0.32);
+  containerBackButton.add(overlayBackSign);
+  const backButtonText = this.add.text(0, -140, "Back", {
+      fontFamily: "WBB",
+      fontSize: 120,
+      color: "#492807",
+  });
+  backButtonText.setRotation(-0.05);
+  containerBackButton.add(backButtonText);
+
+  containerBackButton.setSize(overlayBackSign.displayWidth, overlayBackSign.displayHeight);
+  containerBackButton.setInteractive(new Phaser.Geom.Rectangle(0, 0, overlayBackSign.displayWidth, overlayBackSign.displayHeight-180), Phaser.Geom.Rectangle.Contains);
+
+  layoutContainer.add([containerBackButton]);
+
+  containerBackButton.setInteractive();
+  containerBackButton.on("pointerdown", () => {
+      layoutContainer.setVisible(false);
+      containerMain.setVisible(true);
+  });
+
+  containerScene.add(layoutContainer);
+};
+    
   const closeSettings = () => {
     this.scene.stop();
     this.scene.resume(data.previousSceneKey);
 };
-
-
-const openLayout = (layoutName: string) => {
-  containerMain.removeAll();
-
-  if (layoutName === "Legend") {
-      const legendBackboard = this.add.image(0, -100, "backboard").setDisplaySize(300, 100);
-      const guideBackboard = this.add.image(0, 100, "backboard").setDisplaySize(300, 100);
-      const legendText = this.add.text(0, -100, "Legend", { fontFamily: "WBB", fontSize: 64, color: "#ffffff" }).setOrigin(0.5);
-      const guideText = this.add.text(0, 100, "Guide", { fontFamily: "WBB", fontSize: 64, color: "#ffffff" }).setOrigin(0.5);
-
-      containerMain.add([legendBackboard, guideBackboard, legendText, guideText]);
-  } else if (layoutName === "Settings") {
-      const settingsBackboard = this.add.image(0, 0, "backboard").setDisplaySize(300, 100);
-      const settingsText = this.add.text(0, 0, "Settings", { fontFamily: "WBB", fontSize: 64, color: "#ffffff" }).setOrigin(0.5);
-
-      containerMain.add([settingsBackboard, settingsText]);
-  }
-};
-
-backboardTexts.forEach((text, index) => {
-  const yOffset = getBackboardsOffset(index);
-  const backboard = this.add.image(0, yOffset, "backboard")
-  backboard.setDisplaySize(backboard.width * 3, backboard.height * 2.2);
-
-  const backboardText = this.add.text(0, yOffset, text, {
-      fontFamily: "WBB",
-      fontSize: 125,
-      color: "#ffffff",
-      align: "center",
-  }).setOrigin(0.5);
-
-  containerMain.add(backboard);
-  containerMain.add(backboardText);
-
-  backboard.setInteractive();
-  backboard.on("pointerdown", () => {
-      if (text === "Close options") {
-          closeSettings();
-      } else if (text === "Legend") {
-          openLayout(text);
-      } else if (text === "Settings") {
-          openLayout(text);
-      } else if (text === "Leave game") {
-          closeSettings(); // this should work when game is there
-      }
-  });
-});
-
-
-
-    // const soundIcon = this.add.image(0, 0, "soundIcon")
-    // const muteIcon = this.add.image(0, 0, "muteIcon")
-    // containerMain.add(muteIcon);
-    // containerMain.add(soundIcon);
-    
-    containerScene.add(containerMain);
 
     let lastScene = data.previousSceneKey
     this.input.keyboard!.on('keydown-ESC', (event: Event) => {
